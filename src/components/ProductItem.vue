41ec03093834b75f3e8873026b982de311bb8884
MAAB1707 <abblemni@gmail.com>
2022-11-07 17:22:41 +0500

    vue1

---
+++ src/components/ProductItem.vue
@@ -0,0 +1,47 @@
+<template>
+	<li class="catalog__item">
+            <router-link class="catalog__pic" href="#" :to="{name: 'product', params: {id: product.id}}">
+              <img :src="product.image" :alt="product.title">
+            </router-link>
+
+            <h3 class="catalog__title">
+              <a href="#">
+                {{product.title}}
+              </a>
+            </h3>
+
+            <span class="catalog__price">
+              {{product.price | numberFormat}}
+            </span>
+
+            <ul class="colors colors--black">
+              <li class="colors__item" v-for="colori in product.colors" :key="colori.id">
+                <label class="colors__label">
+                  <input class="colors__radio sr-only" type="radio" :value="colori.id" v-model="color">
+                  <span class="colors__value" :style="{'background-color': colori.code}">
+                  </span>
+                </label>
+              </li>              
+            </ul>
+          </li>
+</template>
+
+<script>
+import gotoPage from "@/helpers/gotoPage"
+import numberFormat from "@/helpers/numberFormat"
+
+	export default {
+		data(){
+			return {
+				color: '#73B6EA'
+			}
+		},
+		filters: {
+			numberFormat
+		},
+		methods: {
+			gotoPage
+		},
+		props:['product']
+	};
+</script>
---
+++ src/data/products.js
@@ -0,0 +1,107 @@
+export default [
+	{
+		id: 1,
+		categoryId: 4,
+		title: 'Радионяня Motorola MBP16',
+		price: 3690,
+		image: '/img/radio.jpg'
+	},
+	{
+		id: 2,
+		categoryId: 3,
+		title: 'KiA better car then your car',
+		price: 3690,
+		image: '/img/radio.jpg'
+
+	},
+	{
+		id: 3,
+		categoryId: 1,
+
+		title: 'this nothing doing their ',
+		price: 3690,
+		image: '/img/radio.jpg'
+
+	},
+	{
+		id: 3,
+		categoryId: 2,
+		title: 'good ning',
+		price: 3690,
+		image: '/img/radio.jpg'
+
+	},
+	{
+		id: 4,
+		categoryId: 5,
+		title: 'Радионяня shapalag ',
+		price: 3690,
+		image: '/img/radio.jpg'
+
+	},
+	{
+		id: 5,
+		categoryId: 5,
+		title: 'Радионяня hie',
+		price: 3690,
+		image: '/img/radio.jpg'
+
+	},
+	{
+		id: 6,
+		categoryId: 4,
+		title: 'Радионяня ratata',
+		price: 3699,
+		image: '/img/radio.jpg'
+
+	},
+	{
+		id: 7,
+		categoryId: 1,
+		title: 'Like you be',
+		price: 5590,
+		image: '/img/radio.jpg'
+
+	},
+	{
+		id: 8,
+		categoryId: 3,
+		title: 'My der fr',
+		price: 2690,
+		image: '/img/radio.jpg'
+
+	},
+	{
+		id: 9,
+		categoryId: 2,
+		title: 'My english vary bad',
+		price: 8690,
+		image: '/img/radio.jpg'
+
+	},
+	{
+		id: 10,
+		categoryId: 5,
+		title: 'whats about you',
+		price: 2690,
+		image: '/img/radio.jpg'
+
+	},
+	{
+		id: 11,
+		categoryId: 1,
+		title: 'its nothin bat',
+		price: 390,
+		image: '/img/radio.jpg'
+
+	},
+	{
+		id: 12,
+		categoryId: 4,
+		title: 'you can be',
+		price: 3550,
+		image: '/img/radio.jpg'
+
+	},
+	
+]
